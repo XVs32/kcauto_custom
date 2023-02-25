91e440259bb54781f92f44910be9d7090f5c89f9
XVs32 <makaut1995.cs02@g2.nctu.edu.tw>
2023-02-26 02:24:49 +0900

    Minor name fix

--- kcauto/cui/pvp.py
+++ kcauto/cui/pvp.py
@@ -9,7 +9,7 @@ def pop_up_menu(stdscr, panel, preset_id):
         for i in range(0, 4):
 
             if i == 0:
-                preset = "Disable"
+                preset = "disable PVP"
             else:
                 preset = str(i)
 
