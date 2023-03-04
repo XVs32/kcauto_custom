1a51046e17c95bbf1f077c359fdf7b38ade30f26
XVs32 <makaut1995.cs02@g2.nctu.edu.tw>
2023-03-04 12:00:25 +0900

    Logic fix

--- kcauto/cui/passive_repair.py
+++ kcauto/cui/passive_repair.py
@@ -3,9 +3,9 @@ from cui.macro import *
 
 def set_config(config, reserve_slot):
     if reserve_slot == 0:
-        config["passive_repair.enabled"] = False
-    else:
         config["passive_repair.enabled"] = True
+    else:
+        config["passive_repair.enabled"] = False
      
     config["passive_repair.slots_to_reserve"] = reserve_slot
 
