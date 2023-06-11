aa7dbaca21c96669315735ffe41c6d7b514a86bc
XVs32 <makaut1995.cs02@g2.nctu.edu.tw>
2023-06-11 20:15:21 +0900

    Add config override

--- kcauto/config/combat.py
+++ kcauto/config/combat.py
@@ -61,6 +61,46 @@ class ConfigCombat(ConfigBase):
         self.clear_stop = config['combat.clear_stop']
         self._override = config['combat.override']
 
+    def config_override(self, config):
+        if "combat.fleet_mode" in config:
+            self.fleet_mode = config['combat.fleet_mode']
+        if "combat.retreat_points" in config:
+            self.retreat_points = config['combat.retreat_points']
+        if "combat.node_selects" in config:
+            self.node_selects = config['combat.node_selects']
+        if "combat.node_formations" in config:
+            self.node_formations = config['combat.node_formations']
+        if "combat.node_night_battles" in config:
+            self.node_night_battles = config['combat.node_night_battles']
+        if "combat.push_nodes" in config:
+            self.push_nodes = config['combat.push_nodes']
+        if "combat.retreat_limit" in config:
+            self.retreat_limit = config['combat.retreat_limit']
+        if "combat.repair_limit" in config:
+            self.repair_limit = config['combat.repair_limit']
+        if "combat.repair_timelimit_hours" in config:
+            self.repair_timelimit_hours = config['combat.repair_timelimit_hours']
+        if "combat.repair_timelimit_minutes" in config:
+            self.repair_timelimit_minutes = config['combat.repair_timelimit_minutes']
+        if "combat.lbas_groups" in config:
+            self.lbas_groups = config['combat.lbas_groups']
+        if "combat.lbas_group_1_nodes" in config:
+            self.lbas_group_1_nodes = config['combat.lbas_group_1_nodes']
+        if "combat.lbas_group_2_nodes" in config:
+            self.lbas_group_2_nodes = config['combat.lbas_group_2_nodes']
+        if "combat.lbas_group_3_nodes" in config:
+            self.lbas_group_3_nodes = config['combat.lbas_group_3_nodes']
+        if "combat.check_fatigue" in config:
+            self.check_fatigue = config['combat.check_fatigue']
+        if "combat.check_lbas_fatigue" in config:
+            self.check_lbas_fatigue = config['combat.check_lbas_fatigue']
+        if "combat.reserve_repair_dock" in config:
+            self.reserve_repair_dock = config['combat.reserve_repair_dock']
+        if "combat.port_check" in config:
+            self.port_check = config['combat.port_check']
+        if "combat.clear_stop" in config:
+            self.clear_stop = config['combat.clear_stop']
+
     @property
     def enabled(self):
         return self._enabled
