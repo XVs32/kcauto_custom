69455de85e731bfd084451b3be3698bd483a0c72
XVs32 <makaut1995.cs02@g2.nctu.edu.tw>
2024-01-21 23:49:23 +0800

    Add --load-equipment option

--- kcauto/args/args_core.py
+++ kcauto/args/args_core.py
@@ -48,6 +48,9 @@ class ArgsCore(object):
         self.parser.add_argument(
             '--save-equipment', action='store_true',
             help="save current equipment config")
+        self.parser.add_argument(
+            '--load-equipment', type=str, action='store',
+            help="load current equipment config")
 
     def parse_args(self):
         self.parsed_args = self.parser.parse_args()
