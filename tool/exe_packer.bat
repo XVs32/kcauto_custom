3811eff22babcec65142dd2cfc2b9ec2be16fc81
xvs32 <makaut1995.cs02@g2.nctu.edu.tw>
2025-06-14 18:11:50 +0800

    pyvisauto update

--- tool/exe_packer.bat
+++ tool/exe_packer.bat
@@ -11,7 +11,7 @@ for /f "delims=" %%i in ('python -c "import site; print(site.getsitepackages()[0
 echo The Python site-packages directory is: %python_site_package%
 
 :: Build the first executable
-python -m PyInstaller -F %src% -p ..\kcauto\ -p %python_site_package%
+python -m PyInstaller -F %src% -p ..\kcauto\ -p ..\pyvisauto\ -p %python_site_package%
 move /y ".\dist\__main__.exe" "%target_path%"
 rmdir /s /q ".\dist"
 rmdir /s /q ".\build"
@@ -20,7 +20,7 @@ del .\__main__.spec
 :: Build the second executable
 set "src=..\kcauto\kcauto_cui.py"
 set "target_path=..\kcauto_cui.exe"
-python -m PyInstaller -F %src% -p ..\kcauto\ -p %python_site_package%
+python -m PyInstaller -F %src% -p ..\kcauto\ -p ..\pyvisauto\ -p %python_site_package%
 move /y ".\dist\kcauto_cui.exe" "%target_path%"
 rmdir /s /q ".\dist"
 rmdir /s /q ".\build"
