5e48c251e3e84620871046ffb44bbfa917feea99
xvs32 <makaut1995.cs02@g2.nctu.edu.tw>
2025-07-09 10:21:39 +0800

    reverse to single file compile, remove bin file

--- tool/x_packer.sh
+++ tool/x_packer.sh
@@ -7,32 +7,32 @@ VENV_PATH="../.venv"
 source "${VENV_PATH}/bin/activate"
 
 SRC="../kcauto/__main__.py"
-TARGET_PATH="../bin/kcauto_custom"
-
-#remove old binaries
-rm -rf ../bin
-rm ../kcauto_custom
-rm ../kcauto_cui
+TARGET_PATH="../"
+rm -r "../kcauto_custom"
 
 # Get the site-packages path from the virtual environment
 PYTHON_SITE_PACKAGE=$(python -c "import site; print(site.getsitepackages()[0])")
 echo "The Python site-packages directory is: $PYTHON_SITE_PACKAGE"
 
+
 # Build the first executable
-python -m PyInstaller --clean $SRC -p ../kcauto -p $PYTHON_SITE_PACKAGE --distpath $TARGET_PATH
-mv "$TARGET_PATH/__main__/__main__" "$TARGET_PATH/__main__/kcauto_custom"
+python -m PyInstaller -F --clean $SRC -p ../kcauto -p $PYTHON_SITE_PACKAGE --distpath $TARGET_PATH --name "kcauto_custom"
+rm -r dist
 rm -r build
 rm __main__.spec
-ln -s "./bin/kcauto_custom/__main__/kcauto_custom" ../kcauto_custom
 
+strip "../kcauto_custom"
 
 # Build the second executable
 SRC="../kcauto/kcauto_cui.py"
-TARGET_PATH="../bin/kcauto_cui"
-python -m PyInstaller --clean $SRC -p ../kcauto/ -p $PYTHON_SITE_PACKAGE --distpath $TARGET_PATH
+TARGET_PATH="../"
+rm -r "../kcauto_cui"
+python -m PyInstaller -F --clean $SRC -p ../kcauto/ -p $PYTHON_SITE_PACKAGE --distpath $TARGET_PATH --name "kcauto_cui"
+rm -r dist
 rm -r build
 rm kcauto_cui.spec
-ln -s "./bin/kcauto_cui/kcauto_cui/kcauto_cui" ../kcauto_cui
+
+strip "../kcauto_cui"
 
 # Deactivate the virtual environment
 deactivate
