b15f3d13391d3858ea5aa11daced976ef3ca516a
xvs32 <makaut1995.cs02@g2.nctu.edu.tw>
2025-07-17 13:39:05 +0800

    Add large ship construction support

--- kcauto/factory/factory_core.py
+++ kcauto/factory/factory_core.py
@@ -117,7 +117,10 @@ class FactoryCore(object):
     def build(self, oil, ammo, steel, bauxite, count):
         """Place the build order"""
         """Assume currently at factory page when called"""
-
+        
+        init_resource = [30, 30, 30, 30]
+        step_multiplier = 1
+            
         while count > 0:
 
             kca_u.kca.sleep(1)
@@ -175,21 +178,28 @@ class FactoryCore(object):
                         self.enabled = False
                         return False
 
+                    if self.is_large_ship_construction(oil, ammo, steel, bauxite) == True:
+                        kca_u.kca.click_existing("lower", "factory|large_ship_construction.png")
+                        kca_u.kca.click_existing("lower", "factory|large_ship_construction_agree.png")
+                        init_resource = [1500, 1500, 2000, 1000]
+                        step_multiplier = 10
+                        
                     resource_list = [oil, ammo, steel, bauxite]
 
                     for i in range(4):
                         """The init 30 point of resource on the order"""
                         resource = resource_list[i]
-                        resource -= 30
-                        while resource >= 100:
+                        resource -= init_resource[i]
+                        
+                        while resource >= 100 * step_multiplier:
                             kca_u.kca.r[self.order_resource_region[i][100]].click()
-                            resource -= 100
-                        while resource >= 10:
+                            resource -= 100 * step_multiplier 
+                        while resource >= 10 * step_multiplier:
                             kca_u.kca.r[self.order_resource_region[i][10]].click()
-                            resource -= 10
-                        while resource >= 1:
+                            resource -= 10 * step_multiplier
+                        while resource >= 1 * step_multiplier:
                             kca_u.kca.r[self.order_resource_region[i][1]].click()
-                            resource -= 1
+                            resource -= 1 * step_multiplier
 
                     kca_u.kca.r["order_confirm_region"].click()
                     kca_u.kca.wait('lower', 'factory|factory_init.png', 20)
@@ -216,5 +226,11 @@ class FactoryCore(object):
         return oil, ammo, steel, bauxite
 
 
+    def is_large_ship_construction(self, oil, ammo, steel, bauxite):
+        """Check if the large ship construction is enabled"""
+        if oil > 999 or ammo > 999 or steel > 999 or bauxite > 999:
+            return True
+        
+        return False
 
 factory = FactoryCore()
