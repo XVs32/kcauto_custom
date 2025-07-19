316e77a4552278ac885aac689fdd34a3b7eb6192
xvs32 <makaut1995.cs02@g2.nctu.edu.tw>
2025-07-19 12:48:10 +0800

    Add quest page switch

--- kcauto/nav/nav.py
+++ kcauto/nav/nav.py
@@ -140,20 +140,59 @@ class NavigateList(object):
     # offset of navigation controls, based off of the ship comp UI's ship list,
     # in x, y pixel format
     
-    OFFSET_MODE_SHIPCOMP = 'shipcomp'
-    OFFSET_MODE_REPAIR = 'repair'
-    OFFSET_MODE_EQUIPMENT = 'equipment'
+    OP_MODE_SHIPCOMP = 'shipcomp'
+    OP_MODE_REPAIR = 'repair'
+    OP_MODE_EQUIPMENT = 'equipment'
+    OP_MODE_QUEST = 'quest'
 
     OFFSET = {
-        OFFSET_MODE_REPAIR: (0, 0),
-        OFFSET_MODE_SHIPCOMP: (17, -4),
-        OFFSET_MODE_EQUIPMENT: (-3, -4)
+        OP_MODE_REPAIR: (0, 0),
+        OP_MODE_SHIPCOMP: (17, -4),
+        OP_MODE_EQUIPMENT: (-3, -4)
     }
-
+    
+    REGION = {
+        OP_MODE_REPAIR: {"first": (735 - (50*2) + OFFSET[OP_MODE_REPAIR][0], 675 + OFFSET[OP_MODE_REPAIR][1]),
+                             "prev": (735 - (50*1) + OFFSET[OP_MODE_REPAIR][0], 675 + OFFSET[OP_MODE_REPAIR][1]),
+                             "next": (735 + (215) + (50*1) + OFFSET[OP_MODE_REPAIR][0], 675 + OFFSET[OP_MODE_REPAIR][1]),
+                             "last": (735 + (215) + (50*2) + OFFSET[OP_MODE_REPAIR][0], 675 + OFFSET[OP_MODE_REPAIR][1]),
+                             1: (739 + (0 * 53) + OFFSET[OP_MODE_REPAIR][0], 675 + OFFSET[OP_MODE_REPAIR][1]),
+                             2: (739 + (1 * 53) + OFFSET[OP_MODE_REPAIR][0], 675 + OFFSET[OP_MODE_REPAIR][1]),
+                             3: (739 + (2 * 53) + OFFSET[OP_MODE_REPAIR][0], 675 + OFFSET[OP_MODE_REPAIR][1]),
+                             4: (739 + (3 * 53) + OFFSET[OP_MODE_REPAIR][0], 675 + OFFSET[OP_MODE_REPAIR][1]),
+                             5: (739 + (4 * 53) + OFFSET[OP_MODE_REPAIR][0], 675 + OFFSET[OP_MODE_REPAIR][1])},
+        OP_MODE_SHIPCOMP: {"first": (735 - (50*2) + OFFSET[OP_MODE_SHIPCOMP][0], 675 + OFFSET[OP_MODE_SHIPCOMP][1]),
+                                "prev": (735 - (50*1) + OFFSET[OP_MODE_SHIPCOMP][0], 675 + OFFSET[OP_MODE_SHIPCOMP][1]),
+                                "next": (735 + (215) + (50*1) + OFFSET[OP_MODE_SHIPCOMP][0], 675 + OFFSET[OP_MODE_SHIPCOMP][1]),
+                                "last": (735 + (215) + (50*2) + OFFSET[OP_MODE_SHIPCOMP][0], 675 + OFFSET[OP_MODE_SHIPCOMP][1]),
+                                1: (739 + (0 * 53) + OFFSET[OP_MODE_SHIPCOMP][0], 675 + OFFSET[OP_MODE_SHIPCOMP][1]),
+                                2: (739 + (1 * 53) + OFFSET[OP_MODE_SHIPCOMP][0], 675 + OFFSET[OP_MODE_SHIPCOMP][1]),
+                                3: (739 + (2 * 53) + OFFSET[OP_MODE_SHIPCOMP][0], 675 + OFFSET[OP_MODE_SHIPCOMP][1]),
+                                4: (739 + (3 * 53) + OFFSET[OP_MODE_SHIPCOMP][0], 675 + OFFSET[OP_MODE_SHIPCOMP][1]),
+                                5: (739 + (4 * 53) + OFFSET[OP_MODE_SHIPCOMP][0], 675 + OFFSET[OP_MODE_SHIPCOMP][1])},
+        OP_MODE_EQUIPMENT: {"first": (735 - (50*2) + OFFSET[OP_MODE_EQUIPMENT][0], 675 + OFFSET[OP_MODE_EQUIPMENT][1]),
+                                "prev": (735 - (50*1) + OFFSET[OP_MODE_EQUIPMENT][0], 675 + OFFSET[OP_MODE_EQUIPMENT][1]),
+                                "next": (735 + (215) + (50*1) + OFFSET[OP_MODE_EQUIPMENT][0], 675 + OFFSET[OP_MODE_EQUIPMENT][1]),
+                                "last": (735 + (215) + (50*2) + OFFSET[OP_MODE_EQUIPMENT][0], 675 + OFFSET[OP_MODE_EQUIPMENT][1]),
+                                1: (739 + (0 * 53) + OFFSET[OP_MODE_EQUIPMENT][0], 675 + OFFSET[OP_MODE_EQUIPMENT][1]),
+                                2: (739 + (1 * 53) + OFFSET[OP_MODE_EQUIPMENT][0], 675 + OFFSET[OP_MODE_EQUIPMENT][1]),
+                                3: (739 + (2 * 53) + OFFSET[OP_MODE_EQUIPMENT][0], 675 + OFFSET[OP_MODE_EQUIPMENT][1]),
+                                4: (739 + (3 * 53) + OFFSET[OP_MODE_EQUIPMENT][0], 675 + OFFSET[OP_MODE_EQUIPMENT][1]),
+                                5: (739 + (4 * 53) + OFFSET[OP_MODE_EQUIPMENT][0], 675 + OFFSET[OP_MODE_EQUIPMENT][1])},
+        OP_MODE_QUEST: {"first": (385, 687),
+                             "prev": (435 , 687),
+                             "next": (920, 687),
+                             "last": (970, 687),
+                             1: (524, 687),
+                             2: (606, 687),
+                             3: (688, 687),
+                             4: (770, 687),
+                             5: (852, 687)}}
+        
     @classmethod
     def to_page(
-            cls, region, page_count, current_page, target_page,
-            offset_mode='shipcomp'):
+            cls, page_count, current_page, target_page,
+            op_mode='shipcomp'):
         """Method that navigates the shiplist to the specified target page from
         the specified current page. Uses _change_page for navigation.
 
@@ -176,52 +215,50 @@ class NavigateList(object):
             if target_page == 1:
                 # shortcut for first page
                 #Log.log_error("first")
-                cls._change_page(region, 'first', cls.OFFSET[offset_mode])
+                cls._change_page('first', op_mode)
                 current_page = 1
             elif target_page == page_count:
                 # shortcut for last page
                 #Log.log_error("last")
-                cls._change_page(region, 'last', cls.OFFSET[offset_mode])
+                cls._change_page('last', op_mode)
                 current_page = page_count
             elif target_page <= 5 and (current_page <= 3 or page_count <= 5):
                 #Log.log_error("direct 1")
-                cls._change_page(region, target_page, cls.OFFSET[offset_mode])
+                cls._change_page(target_page, op_mode)
                 current_page = target_page
             elif (current_page >= page_count - 2
                     and target_page >= page_count - 4):
                 #Log.log_error("direct 2")
                 cls._change_page(
-                    region, abs(page_count - target_page - 5),
-                    cls.OFFSET[offset_mode])
+                    abs(page_count - target_page - 5), op_mode)
                 current_page = target_page
             elif -3 < page_delta < 3:
                 #Log.log_error("direct 3")
-                cls._change_page(
-                    region, 3 + page_delta, cls.OFFSET[offset_mode])
+                cls._change_page( 3 + page_delta, op_mode)
                 current_page = current_page + page_delta
             elif page_delta <= - 3:
                 if target_page <= 5:
                     #Log.log_error("back to first")
-                    cls._change_page(region, 'first', cls.OFFSET[offset_mode])
+                    cls._change_page('first', op_mode)
                     current_page = 1
                 else:
                     #Log.log_error("prev")
-                    cls._change_page(region, 'prev', cls.OFFSET[offset_mode])
+                    cls._change_page('prev', op_mode)
                     current_page -= 5
             elif page_delta >= 3:
                 if target_page > (page_count - 5):
                     #Log.log_error("go to last")
-                    cls._change_page(region, 'last', cls.OFFSET[offset_mode])
+                    cls._change_page('last', op_mode)
                     current_page = page_count
                 else:
                     #Log.log_error("next")
-                    cls._change_page(region, 'next', cls.OFFSET[offset_mode])
+                    cls._change_page('next', op_mode)
                     current_page += 5
         kca_u.kca.sleep(0.5)
         return current_page
 
-    @staticmethod
-    def _change_page(region, target, offset):
+    @classmethod
+    def _change_page(cls, target, mode):
         """Method that clicks on the arrow and page number navigation at the
         bottom of the ship list. 'first', 'prev', 'next', 'last' targets will
         click their respective arrow buttons, while an int target between 1 and
@@ -234,29 +271,16 @@ class NavigateList(object):
             offset (tuple): tuple of x, y pixel offsets as defined by
                 offset_mode and the OFFSET class dictionary
         """
-        Log.log_debug(f"Changing to page {target} with {offset} offsets.")
-        if target == 'first':
-            x_start = kca_u.kca.game_x + 735 - (50*2) + offset[0]
-            y_start = kca_u.kca.game_y + 675 + offset[1]
+        Log.log_debug(f"Changing to page {target} with {mode} offsets.")
+        x_start = kca_u.kca.game_x + cls.REGION[mode][target][0]
+        y_start = kca_u.kca.game_y + cls.REGION[mode][target][1]
+            
+        if type(target) is str and target.isdigit():
             Region(x_start, y_start, 16, 15).click()
-        elif target == 'prev':
-            x_start = kca_u.kca.game_x + 735 - (50*1) + offset[0]
-            y_start = kca_u.kca.game_y + 675 + offset[1]
-            Region(x_start, y_start, 16, 15).click()
-        elif target == 'next':
-            x_start = kca_u.kca.game_x + 735 + (215) + (50*1) + offset[0]
-            y_start = kca_u.kca.game_y + 675 + offset[1]
-            Region(x_start, y_start, 16, 15).click()
-        elif target == 'last':
-            x_start = kca_u.kca.game_x + 735 + (215) + (50*2) + offset[0]
-            y_start = kca_u.kca.game_y + 675 + offset[1]
-            Region(x_start, y_start, 16, 15).click()
-        elif 1 <= target <= 5:
-            zero_target = target - 1
-            x_start = kca_u.kca.game_x + 739 + (zero_target * 53) + offset[0]
-            y_start = kca_u.kca.game_y + 675 + offset[1]
-
+        else:
             Region(x_start, y_start, 10, 15).click()
+            
+
         kca_u.kca.sleep(0.5)
 
 
