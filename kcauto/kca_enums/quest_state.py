486401aa191135da68199e1bfe82921fa15948bd
xvs32 <makaut1995.cs02@g2.nctu.edu.tw>
2025-07-19 12:47:32 +0800

    Add quest emun

---
+++ kcauto/kca_enums/quest_state.py
@@ -0,0 +1,6 @@
+from kca_enums.enum_base import EnumBase
+
+class QuestStateEnum(EnumBase):
+    IDLE = 1
+    IN_PROGRESS = 2
+    DONE = 3
---
+++ kcauto/kca_enums/quest_type.py
@@ -0,0 +1,9 @@
+from kca_enums.enum_base import EnumBase
+
+
+class QuestTypeEnum(EnumBase):
+    DAILY = 1
+    WEEKLY = 2
+    MONTHLY = 3
+    SINGLE = 4
+    OTHER = 5
