4e795a9ad5d87ab111f19fd25c4abbacf37768e4
xvs32 <makaut1995.cs02@g2.nctu.edu.tw>
2025-09-05 15:01:19 +0800

    Handle monthly expedition

--- kcauto/startup/kcauto.py
+++ kcauto/startup/kcauto.py
@@ -90,6 +90,7 @@ class Kcauto(object):
                     
                 exp.expedition.prerequisite_handling()
                 exp.expedition.on_going_exp_handling()
+                exp.expedition.monthly_exp_handling()
                 
                 Log.log_msg(f'Expedition rank: {[expedition[exp.expedition.EXP_ENUM].display_name for expedition in exp.expedition.exp_rank]}')
 
