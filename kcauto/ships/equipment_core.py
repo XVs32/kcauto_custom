182a1c7873568667dda6fdb11ab7890012fa8d33
xvs32 <makaut1995.cs02@g2.nctu.edu.tw>
2025-09-09 20:14:51 +0800

    Add goto and fleet control

--- kcauto/ships/equipment_core.py
+++ kcauto/ships/equipment_core.py
@@ -9,6 +9,8 @@ from util.logger import Log
 from kca_enums.ship_class import ShipClassEnum 
 from kca_enums.ship_types import ShipTypeEnum
 
+import nav.nav as nav
+import util.kca as kca_u
 
 class EquipmentCore(object):
     
@@ -57,6 +59,27 @@ class EquipmentCore(object):
         except FileNotFoundError:
             Log.log_error("Equipment data not found, please start kcauto from splash screen")
             Log.log_error(e)
+            
+    def goto(self):
+        nav.navigate.to('equipment')
+        
+    def goto_fleet(self, fleet_id):
+        """method to navigate to the fleet in equipment page, page 'other' as treated as fleet 999
+
+        Args:
+            fleet_id (_type_): _description_
+        """
+        
+        clicked = False
+        
+        kca_u.kca.wait("left", f"nav|side_menu_equipment_active.png")
+        while True:
+            clicked = kca_u.kca.click_existing("upper_left", f"fleet|fleet_{fleet_id}.png")
+            if  kca_u.kca.exists("upper_left", f"fleet|fleet_{fleet_id}_active.png"):
+                break
+            kca_u.kca.sleep(1)
+        
+        return clicked
             
     def _remove_from_pool(self, target_equipment : Equipment, pool):
         
