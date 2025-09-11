4a4e52b4b839e8613f4def1dfc795a72981307a0
xvs32 <makaut1995.cs02@g2.nctu.edu.tw>
2025-09-11 23:50:19 +0800

    Add factory panel

---
+++ kcauto/cui/factory.py
@@ -0,0 +1,295 @@
+import curses
+
+import cui.util as util
+from cui.macro import *
+from constants import CONTEXT_SORTIE, CONTEXT_EXPEDITION, CONTEXT_PVP, CONTEXT_FACTORY, CONTEXT_REPAIR
+
+from fleet.noro6 import Noro6 
+
+CONSTRUCT = 1
+DEVELOP = 2
+
+TOP_MENU = 1
+SECRETARY_MENU = 2
+PRESET_MENU = 3
+FUEL_MENU = 4
+AMMO_MENU = 5
+STEEL_MENU = 6
+BAUXITE_MENU = 7
+
+SECRETARY_MODE_ID = 1
+SECRETARY_MODE_TYPE = 2
+
+MAX_QUEST_COL = 16
+
+TAB_ORDER = [CONSTRUCT, DEVELOP]
+RESOURCE_ORDER = [FUEL_MENU, AMMO_MENU, STEEL_MENU, BAUXITE_MENU]
+RESOURCE_PANEL_LAYOUT = [
+    [FUEL_MENU, STEEL_MENU],
+    [AMMO_MENU, BAUXITE_MENU]
+]
+
+RESOURCE_DISPLAY_NAME = {
+    FUEL_MENU: "Fuel",
+    AMMO_MENU: "Ammo",
+    STEEL_MENU: "Steel",
+    BAUXITE_MENU: "Bauxite"
+}
+
+BOARDER_WIDTH = 1
+COL_NEEDED_WIDTH = 5
+
+CURSER_X = 0
+CURSER_Y = 1
+
+quest_info = []
+
+def int_to_list(n, length):
+    ret = []
+    while n > 0:
+        ret.append(n % 10)
+        n //= 10
+    while len(ret) < length:
+        ret.append(0)
+    ret.reverse()
+    return ret
+
+def list_to_int(lst):
+    if type(lst) != list:
+        return lst
+    ret = 0
+    for n in lst:
+        ret = ret * 10 + n
+    return ret
+
+def pop_up_menu(stdscr, panel, config):
+    
+    global quest_info
+    if quest_info == []:
+        with open('data/quests/kc3_periodic_quests_en.json', 'r', encoding='utf-8') as f:
+            import json
+            quest_info = json.load(f)    
+    
+    current_tab = CONSTRUCT 
+    current_active = TOP_MENU 
+    curser = [1, 0]
+    
+    height, width = panel.getmaxyx()
+    
+    tab_height = 1
+    secretary_height = 2
+    resource_height = 4
+    
+    row = [0, tab_height, tab_height + secretary_height]
+    
+    #read current quest status from config
+    recipe = {}
+    recipe[CONSTRUCT] = config["factory.build_recipe"]
+    recipe[DEVELOP] = config["factory.develop_recipe"]
+    
+    for tab in TAB_ORDER:
+        for i in range(len(recipe[tab])):
+            recipe[tab][i] = int_to_list(recipe[tab][i], 4)
+            
+    print(recipe)
+    
+    secretary = {}
+    secretary[CONSTRUCT] = int_to_list(config["factory.build_secretary"], 7)
+    secretary[DEVELOP] = int_to_list(config["factory.develop_secretary"], 7)
+    
+    y_offset = 0
+    
+    tab_col = [width//2//2 - len(" construct ")//2, width//2 + width//2//2 - len(" develop ")//2]
+    
+    x_secretary, y_secretary = util.get_center_str_location(panel, "Secretary ship XXXX XXXXXXX")
+    secretary_col = [x_secretary, x_secretary + len("Secretary ship "), x_secretary + len("Secretary ship XXXX ")]
+    
+    x_recipe, y_recipe = util.get_center_str_location(panel, "PRESETNAME  XAMMOX XXXX  BAUXITE  XXXX")
+    recipe_col = [x_recipe, 
+                  x_recipe + len("PRESETNAME    "), 
+                  x_recipe + len("PRESETNAME    XAMMOX "), 
+                  x_recipe + len("PRESETNAME    XAMMOX XXXX  "), 
+                  x_recipe + len("PRESETNAME    XAMMOX XXXX  BAUXITE ")]
+    
+    secretary_mode = SECRETARY_MODE_ID
+    
+    while True:
+        
+        panel.clear()
+        panel.border()
+        panel.addstr(row[0], tab_col[0], 
+                    ("<" if curser[CURSER_Y] == 0 and current_tab == CONSTRUCT else " ")+"Construct"+(">" if curser[CURSER_Y] == 0 and current_tab == CONSTRUCT else " "),
+                    curses.color_pair(SORTIE + (COLOR_REVERT * (int(current_tab == CONTEXT_SORTIE)))))
+        panel.addstr(row[0], tab_col[1], 
+                    ("<" if curser[CURSER_Y] == 0 and current_tab == DEVELOP else " ")+"Develop"+(">" if curser[CURSER_Y] == 0 and current_tab == DEVELOP else " "),
+                    curses.color_pair(PVP + (COLOR_REVERT * (int(current_tab == CONTEXT_PVP)))))
+        
+        panel.addstr(row[1] + 1, secretary_col[0], "Secretary ship ", curses.color_pair(LOG))
+        
+        panel.addstr(row[1] + 1, secretary_col[1], " ID ", curses.color_pair(LOG))
+        
+        if current_active == SECRETARY_MENU:
+            for i in range(7):
+                if curser[CURSER_Y] == 1 and i == curser[CURSER_X]:
+                    panel.addstr(row[1] + 0, secretary_col[2] + i, str((secretary[current_tab][i] +9) % 10), curses.color_pair(LOG))
+                    panel.addstr(row[1] + 1, secretary_col[2] + i, str(secretary[current_tab][i]), curses.color_pair(LOG_GREEN))
+                    panel.addstr(row[1] + 2, secretary_col[2] + i, str((secretary[current_tab][i] +1) % 10), curses.color_pair(LOG))
+                else:    
+                    panel.addstr(row[1] + 1, secretary_col[2] + i, str(secretary[current_tab][i]), curses.color_pair(LOG))
+        else:
+            
+            if curser[CURSER_Y] == 1:
+                panel.addstr(row[1] + 1, secretary_col[2], str(list_to_int(secretary[current_tab])), curses.color_pair(LOG_GREEN))
+            else:
+                panel.addstr(row[1] + 1, secretary_col[2], str(list_to_int(secretary[current_tab])), curses.color_pair(LOG))
+        
+        for resource_idx, resource in enumerate(RESOURCE_ORDER):
+            
+            focus = None
+            if resource == FUEL_MENU:
+                row_local_offset = 0
+                col_offset = 1
+                color = PVP
+                if curser[CURSER_Y] == 2 and curser[CURSER_X] == 1:
+                    focus = FUEL_MENU
+                
+            elif resource == AMMO_MENU:
+                row_local_offset = 2
+                col_offset = 1
+                color = FACTORY
+                if curser[CURSER_Y] == 3 and curser[CURSER_X] == 1:
+                    focus = AMMO_MENU
+            elif resource == STEEL_MENU:
+                row_local_offset = 0
+                col_offset = 3
+                color = STEEL
+                if curser[CURSER_Y] == 2 and curser[CURSER_X] == 2:
+                    focus = STEEL_MENU
+            elif resource == BAUXITE_MENU:
+                row_local_offset = 2
+                col_offset = 3
+                color = REPAIR 
+                if curser[CURSER_Y] == 3 and curser[CURSER_X] == 2:
+                    focus = BAUXITE_MENU
+            panel.addstr(row[2] + row_local_offset + 1, recipe_col[col_offset], " "+RESOURCE_DISPLAY_NAME[resource]+" ", curses.color_pair(color))
+        
+            if current_active == resource:    
+                for i in range(4):
+                    if i == curser[CURSER_X]:
+                        panel.addstr(row[2] + row_local_offset + 0, recipe_col[col_offset +1] + i, str((recipe[current_tab][resource_idx][i] +9) % 10), curses.color_pair(LOG))
+                        panel.addstr(row[2] + row_local_offset + 1, recipe_col[col_offset +1] + i, str(recipe[current_tab][resource_idx][i]), curses.color_pair(LOG_GREEN))
+                        panel.addstr(row[2] + row_local_offset + 2, recipe_col[col_offset +1] + i, str((recipe[current_tab][resource_idx][i] +1) % 10), curses.color_pair(LOG))
+                    else:    
+                        panel.addstr(row[2] + row_local_offset + 1, recipe_col[col_offset +1] + i, str(recipe[current_tab][resource_idx][i]), curses.color_pair(LOG))
+            else:
+                if focus == resource:
+                    panel.addstr(row[2] + row_local_offset + 1, recipe_col[col_offset +1], str(list_to_int(recipe[current_tab][resource_idx])).rjust(4, " "), curses.color_pair(LOG_GREEN))
+                else:
+                    panel.addstr(row[2] + row_local_offset + 1, recipe_col[col_offset +1], str(list_to_int(recipe[current_tab][resource_idx])).rjust(4, " "), curses.color_pair(LOG))
+               
+        
+        panel.refresh()
+
+        # Wait for next input
+        key = stdscr.getch()
+        
+        if key == curses.KEY_DOWN or key == ord('j'):
+            if current_active == TOP_MENU:
+                if curser[CURSER_Y] < 3:
+                    curser[CURSER_Y] += 1
+            elif current_active == SECRETARY_MENU:
+                secretary[current_tab][curser[CURSER_X]] = (secretary[current_tab][curser[CURSER_X]] + 1) %10
+            elif current_active == FUEL_MENU or current_active == AMMO_MENU or current_active == STEEL_MENU or current_active == BAUXITE_MENU:
+                recipe[current_tab][RESOURCE_ORDER.index(current_active)][curser[CURSER_X]] \
+                    = (recipe[current_tab][RESOURCE_ORDER.index(current_active)][curser[CURSER_X]] + 1) %10
+                
+        elif key == curses.KEY_UP or key == ord('k'):
+            if current_active == TOP_MENU:
+                if curser[CURSER_Y] > 0:
+                    curser[CURSER_Y] -= 1
+            elif current_active == SECRETARY_MENU:
+                secretary[current_tab][curser[CURSER_X]] = (secretary[current_tab][curser[CURSER_X]] + 9) %10
+            elif current_active == FUEL_MENU or current_active == AMMO_MENU or current_active == STEEL_MENU or current_active == BAUXITE_MENU:
+                recipe[current_tab][RESOURCE_ORDER.index(current_active)][curser[CURSER_X]] \
+                    = (recipe[current_tab][RESOURCE_ORDER.index(current_active)][curser[CURSER_X]] + 9) %10
+                    
+        elif key == curses.KEY_RIGHT or key == ord('l'):
+            
+            if current_active == TOP_MENU:
+                if curser[CURSER_Y] == 0:
+                    if current_tab != TAB_ORDER[-1]:
+                        current_tab = TAB_ORDER[TAB_ORDER.index(current_tab)+1]
+                        curser[CURSER_X] = 1
+                else:
+                    if curser[CURSER_X] < 2:
+                        curser[CURSER_X] += 1
+            elif current_active == SECRETARY_MENU:
+                if curser[CURSER_X] < 6:
+                    curser[CURSER_X] += 1
+            elif current_active == FUEL_MENU or current_active == AMMO_MENU or current_active == STEEL_MENU or current_active == BAUXITE_MENU:
+                if curser[CURSER_X] < 3:
+                    curser[CURSER_X] += 1
+                
+        elif key == curses.KEY_LEFT or key == ord('h'):
+            if current_active == TOP_MENU:
+                if curser[CURSER_Y] == 0:
+                    if current_tab != TAB_ORDER[0]:
+                        current_tab = TAB_ORDER[TAB_ORDER.index(current_tab)-1]
+                        curser[CURSER_X] = 1
+                else:
+                    if curser[CURSER_X] > 0:
+                        curser[CURSER_X] -= 1
+            elif current_active == SECRETARY_MENU:
+                if curser[CURSER_X] > 0:
+                    curser[CURSER_X] -= 1
+            elif current_active == FUEL_MENU or current_active == AMMO_MENU or current_active == STEEL_MENU or current_active == BAUXITE_MENU:
+                if curser[CURSER_X] > 0:
+                    curser[CURSER_X] -= 1
+            
+        elif key == KEY_ENTER:
+            if current_active == TOP_MENU:
+                if curser[CURSER_Y] == 0:
+                    current_tab = TAB_ORDER[curser[CURSER_X]]
+                elif curser[CURSER_Y] == 1:
+                    current_active = SECRETARY_MENU
+                    curser[CURSER_X] = 6
+                    if secretary[current_tab] == 'on-hand':
+                        secretary[current_tab] = [0,0,0,0,0,0,0]
+                elif curser[CURSER_Y] == 2 and curser[CURSER_X] == 0:
+                    #preset
+                    pass
+                else:
+                    current_active = RESOURCE_ORDER[(curser[CURSER_Y]-2) + (curser[CURSER_X]-1)*2]
+                    curser[CURSER_Y] = None
+                    curser[CURSER_X] = 3
+                    
+            elif current_active == SECRETARY_MENU:
+                current_active = TOP_MENU
+                curser[CURSER_X] = 1
+                curser[CURSER_Y] = 1
+                if secretary[current_tab] == [0,0,0,0,0,0,0]:
+                    secretary[current_tab] = 'on-hand'
+            elif current_active == FUEL_MENU or current_active == AMMO_MENU or current_active == STEEL_MENU or current_active == BAUXITE_MENU:
+                curser[CURSER_X] = RESOURCE_ORDER.index(current_active)//2 +1
+                curser[CURSER_Y] = RESOURCE_ORDER.index(current_active)%2 +2
+                current_active = TOP_MENU
+                
+                    
+        elif key == ord('?') or key == KEY_ESC or key == ord('q'):
+            break
+    return None
+
+def set_config(config):
+    return  
+    config["quest.enabled"] = False
+    config["quest.quests"] = []
+    
+    for i, tab in enumerate(TAB_ORDER): 
+        for j, quest_type in enumerate(QUEST_ORDER):
+            for k, quest in enumerate(QUEST_LIST[tab][quest_type]):
+                
+                if QUEST_LIST[tab][quest_type][quest] == 1:
+                    config["quest.enabled"] = True
+                    config["quest.quests"].append(quest)
+    
+    return
