11be6121fcaa5a3c4b97647729a7d2bae3bec185
XVs32 <makaut1995.cs02@g2.nctu.edu.tw>
2026-01-03 16:17:36 +0800

    164 kcauto crash if combat module is disabled and pvp resets (#173)
    * use member method to disable module, instead of accessing private variable directly
    
    * restore log module previous status
    
    * Add enable switch to log module

--- kcauto/startup/kcauto.py
+++ kcauto/startup/kcauto.py
@@ -352,10 +352,12 @@ class Kcauto(object):
         
         if passive_only == True:
             #passive repair only, temporarily disable combat and pvp module
+            log_temp = Log.enabled
+            Log.enabled = False
             combat_temp = com.combat.enabled
-            com.combat._enabled = False
+            com.combat.enabled = False
             pvp_temp = pvp.pvp.enabled
-            pvp.pvp._enabled = False
+            pvp.pvp.enabled = False
         
         if rep.repair.can_conduct_repairs:
             rep.repair.repair_ships()
@@ -368,8 +370,9 @@ class Kcauto(object):
             
         if passive_only == True:
             #restore combat and pvp module status
-            com.combat._enabled = combat_temp
-            pvp.pvp._enabled = pvp_temp
+            com.combat.enabled = combat_temp
+            pvp.pvp.enabled = pvp_temp
+            Log.enabled = log_temp 
             
 
     def _run_fleetswitch_logic(self, context):
