acaad0145aab80e9a6f0dca97037683c04afcdbc
xvs32 <makaut1995.cs02@g2.nctu.edu.tw>
2026-01-12 21:58:17 +0800

    Fix thread handling

--- kcauto/kcauto_cui.py
+++ kcauto/kcauto_cui.py
@@ -333,9 +333,9 @@ def open_pop_up(thread, stdscr, active_panel):
                     output.close()
                     
                     # send a SIGTERM signal to terminate the subprocess
-                    if thread.is_alive() == True:
-                        util.process.send_signal(subprocess.signal.SIGTERM)
-                        thread.join()
+                    if util.psutil_proc and util.psutil_proc.is_running():
+                        util.psutil_proc.kill()
+                        util.psutil_proc.wait(timeout=5)
 
                     thread = kc_auto_kick_start(panels[LOG])
                 break
