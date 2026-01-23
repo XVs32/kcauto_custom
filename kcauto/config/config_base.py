fc203b2d94d3e636956cefa26aba3ef0a47b199a
xvs32 <marco.ma@hukuibio.com>
2026-01-23 17:43:56 +0800

    read and merge user_config and system_config

--- kcauto/config/config_base.py
+++ kcauto/config/config_base.py
@@ -4,8 +4,8 @@ from abc import ABC
 class ConfigBase(ABC):
     _config = {}
 
-    def __init__(self, config):
-        self._config = config
+    def __init__(self, user_config, system_config):
+        self._config = user_config
 
     def __eq__(self, other):
         for prop, value in vars(self).iteritems():
