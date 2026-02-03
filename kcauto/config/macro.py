f7004311f786478f74643ef61aa6babf9c5fc57b
XVs32 <makaut1995.cs02@g2.nctu.edu.tw>
2026-02-03 14:08:32 +0800

    183 recipe default (#184)
    * Add recipe template
    
    * Use macro for file path
    
    * typo fix
    
    * typo fix
    
    ---------

--- kcauto/config/macro.py
+++ kcauto/config/macro.py
@@ -1,3 +1,8 @@
 CONFIG_DEFAULT = 'data/config/config_cui_template.json'
 
 CONFIG_CUI = 'configs/config_cui.json'
+
+RECIPE_PRESET_CONSTRUCT = 'data/factory/construct_recipe_preset.json'
+RECIPE_PRESET_CONSTRUCT_TEMPLATE = 'data/factory/construct_recipe_preset_template.json'
+RECIPE_PRESET_DEVELOP = 'data/factory/develop_recipe_preset.json'
+RECIPE_PRESET_DEVELOP_TEMPLATE = 'data/factory/develop_recipe_preset_template.json'
