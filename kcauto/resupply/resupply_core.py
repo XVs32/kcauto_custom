f41d82c7d7d3b40a9b439598b88a97b30033867c
xvs32 <makaut1995.cs02@g2.nctu.edu.tw>
2026-03-17 21:57:47 +0800

    remove repeat sleep

--- kcauto/resupply/resupply_core.py
+++ kcauto/resupply/resupply_core.py
@@ -39,7 +39,6 @@ class ResupplyCore(object):
                     'upper_left', 'resupply|resupply_all.png')
                 api_result = api.api.update_from_api(
                     {KCSAPIEnum.RESUPPLY_ACTION}, process_all=False, timeout=1)
-                kca_u.kca.sleep()
             sts.stats.resupply.resupplies_done += 1
 
     def exp_provisional_resupply(self, fleet):
