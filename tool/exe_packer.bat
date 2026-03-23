e383300d355ded857ae971e8c2b98d93a4dc198d
XVs32 <makaut1995.cs02@g2.nctu.edu.tw>
2026-03-23 10:40:03 +0800

    pack executable into folder instead of single file, windows version

--- tool/exe_packer.bat
+++ tool/exe_packer.bat
@@ -1,7 +1,8 @@
 @echo off
 set "VENV_PATH=..\.venv"
+set "BIN_PATH=..\bin"
+set "ROOT_PATH=..\"
 set "src=..\kcauto\__main__.py"
-set "target_path=..\kcauto.exe"
 
 :: Activate the virtual environment
 call "%VENV_PATH%\Scripts\activate.bat"
@@ -11,20 +12,25 @@ for /f "delims=" %%i in ('python -c "import site; print(site.getsitepackages()[0
 echo The Python site-packages directory is: %python_site_package%
 
 :: Build the first executable
-python -m PyInstaller -F %src% -p ..\kcauto\ -p %python_site_package%
-move /y ".\dist\__main__.exe" "%target_path%"
+if exist "%BIN_PATH%\kcauto_custom" rmdir /s /q "%BIN_PATH%\kcauto_custom"
+python -m PyInstaller -D --clean %src% -p ..\kcauto\ -p %python_site_package% --distpath %BIN_PATH% --name "kcauto_custom"
 rmdir /s /q ".\dist"
 rmdir /s /q ".\build"
 del .\__main__.spec
+:: Create wrapper in project root
+> "%ROOT_PATH%kcauto_custom.bat" echo @echo off
+>> "%ROOT_PATH%kcauto_custom.bat" echo "%~dp0bin\kcauto_custom\kcauto_custom.exe" %%*
 
 :: Build the second executable
 set "src=..\kcauto\kcauto_cui.py"
-set "target_path=..\kcauto_cui.exe"
-python -m PyInstaller -F %src% -p ..\kcauto\ -p %python_site_package%
-move /y ".\dist\kcauto_cui.exe" "%target_path%"
+if exist "%BIN_PATH%\kcauto_cui" rmdir /s /q "%BIN_PATH%\kcauto_cui"
+python -m PyInstaller -D --clean %src% -p ..\kcauto\ -p %python_site_package% --distpath %BIN_PATH% --name "kcauto_cui"
 rmdir /s /q ".\dist"
 rmdir /s /q ".\build"
 del .\kcauto_cui.spec
+:: Create wrapper in project root
+> "%ROOT_PATH%kcauto_cui.bat" echo @echo off
+>> "%ROOT_PATH%kcauto_cui.bat" echo "%~dp0bin\kcauto_cui\kcauto_cui.exe" %%*
 
 :: Deactivate the virtual environment
 call "%VENV_PATH%\Scripts\deactivate.bat"
