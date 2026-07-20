da9deaa5bd945afa8cf469474c2e4d463cc7c184
XVs32 <makaut1995.cs02@g2.nctu.edu.tw>
2026-07-20 09:27:08 +0800

    Add node type end handling for 5-6 gimmick

--- kcauto/combat/combat_core.py
+++ kcauto/combat/combat_core.py
@@ -413,8 +413,9 @@ class CombatCore(CoreBase):
                 if self.current_node.boss_node or self.boss_api:
                     # Todo: only trigger when screen doesn't change at all
                     kca_u.kca.sleep(8)
-                    Log.log_msg("Dismissing boss dialogue.")
-                    kca_u.kca.r["center"].click()
+                    if not kca_u.kca.exists("lower_right_corner", "global|next.png"):
+                        Log.log_msg("Dismissing boss dialogue.")
+                        kca_u.kca.r["center"].click()
 
                 while not kca_u.kca.exists("lower_right_corner", "global|next.png"):
                     if kca_u.kca.exists("kc", "global|combat_nb_fight.png"):
@@ -467,7 +468,7 @@ class CombatCore(CoreBase):
                     conducting_sortie = False
 
             elif node_type == self.NODE_TYPE_SELECT:
-                Log.log_msg(f"Node selection.")
+                Log.log_msg(f"Node type select {self.current_node.name}.")
                 next_node = cfg.config.combat.node_selects.get(
                     self.current_node.name, None
                 )
@@ -477,10 +478,11 @@ class CombatCore(CoreBase):
                     Log.log_msg(f"Selecting node {next_node}")
                     self.map_data.nodes[next_node].select()
             elif node_type == self.NODE_TYPE_NOTHING:
-                pass
+                Log.log_debug_1(f"Node type nothing {self.current_node.name}.")
             elif node_type == self.NODE_TYPE_END:
+                Log.log_debug_1(f"Node type end {self.current_node.name}.")
                 conducting_sortie = False
-                continue
+            self.gimmick_judge(node_type=node_type)
 
         self._click_until_port()
         Log.log_msg(f"Sortie handling complete.")
@@ -499,7 +501,6 @@ class CombatCore(CoreBase):
                     | self.SHIPDECK_API
                     | self.EQUIP_API,
                     process_all=True,
-                    timeout=3,
                 )
 
                 if KCSAPIEnum.PORT.name not in api_result:
@@ -521,6 +522,7 @@ class CombatCore(CoreBase):
                     | self.EQUIP_API
                     | self.MAP_API,
                     process_all=False,
+                    needed_all=False,
                     timeout=5,
                 )
 
@@ -787,8 +789,6 @@ class CombatCore(CoreBase):
                 f"Battle rank in node {self.current_node}: {self.last_battle[self.RANKENUM].in_str}"
             )
 
-            self.gimmick_judge()
-
     def _calculate_hps(self, new_hps, data):
         for phase in self.API_COMBAT_PHASES_TYPE1:
             if phase in data and data[phase] is not None:
@@ -965,7 +965,7 @@ class CombatCore(CoreBase):
         Log.log_error(f"No gimmick for map {map_enum.value}.")
         return None
 
-    def gimmick_judge(self):
+    def gimmick_judge(self, node_type: int):
 
         if self.gimmick_attampt == None:
             return
@@ -976,26 +976,42 @@ class CombatCore(CoreBase):
         if (
             current_map.without_quest_and_node_enum
             == self.gimmick_attampt.without_quest_and_node_enum
-            and self.last_battle[self.MAP_NODE].name == self.gimmick_attampt.variant
         ):
-            Log.log_msg(f"Battled in gimmick node {self.gimmick_attampt}")
-
-            if self.last_battle[self.RANKENUM].is_at_least(
-                SortieRankEnum[
-                    self.gimmick_list[self.gimmick_attampt.display_name][
-                        self.GIMMICK_MIN_RANK
-                    ]
-                ]
+            if (
+                node_type == self.NODE_TYPE_COMBAT
+                or node_type == self.NODE_TYPE_COMBAT_FINISH
             ):
-                Log.log_success(f"Gimmick solved for map {current_map.value}.")
-                self.gimmick_list[self.gimmick_attampt.display_name][
-                    self.GIMMICK_CLEAR_REMAINING
-                ] -= 1
-                JsonData.dump_json(self.gimmick_list, GIMMICK)
+                if self.last_battle[self.MAP_NODE].name == self.gimmick_attampt.variant:
+                    Log.log_msg(f"Battled in gimmick node {self.gimmick_attampt}")
+
+                    if self.last_battle[self.RANKENUM].is_at_least(
+                        SortieRankEnum[
+                            self.gimmick_list[self.gimmick_attampt.display_name][
+                                self.GIMMICK_MIN_RANK
+                            ]
+                        ]
+                    ):
+                        Log.log_success(f"Gimmick solved for map {current_map.value}.")
+                        self.gimmick_list[self.gimmick_attampt.display_name][
+                            self.GIMMICK_CLEAR_REMAINING
+                        ] -= 1
+                        JsonData.dump_json(self.gimmick_list, GIMMICK)
+
+            elif node_type == self.NODE_TYPE_END:
+                if self.current_node.name == self.gimmick_attampt.variant:
+                    Log.log_msg(f"Reached gimmick node {self.gimmick_attampt}")
+                    self.gimmick_list[self.gimmick_attampt.display_name][
+                        self.GIMMICK_CLEAR_REMAINING
+                    ] -= 1
+                    JsonData.dump_json(self.gimmick_list, GIMMICK)
 
     def gimmick_startup_judge(self, file_name):
 
         for display_name in self.gimmick_list:
+            Log.log_debug_1(
+                f"Checking gimmick for map {display_name}, checking file {self.gimmick_list[display_name][self.GIMMICK_NODE_JSON]}, filename is {file_name}"
+            )
+
             if (
                 self.gimmick_list[display_name][self.GIMMICK_NODE_JSON] == file_name
                 and self.gimmick_list[display_name][self.GIMMICK_CLEAR_REMAINING] > 0
