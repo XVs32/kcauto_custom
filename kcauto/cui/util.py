121f4246a3641031fa3a61c35495f4beb2a0e7b4
XVs32 <makaut1995.cs02@g2.nctu.edu.tw>
2026-07-24 10:31:20 +0800

    Add preset dry run switch

--- kcauto/cui/util.py
+++ kcauto/cui/util.py
@@ -13,6 +13,7 @@ WINDOW_MIN_WIDTH = 45
 POP_UP_MAX_HEIGHT = 20
 
 pop_up_lock = False
+paused_lock = False
 
 log_buffer = []
 
@@ -119,6 +120,10 @@ def run_external_program(panel):
         output = line.strip()
         if output:
             print_log(panel, f"{output}\n")
+
+            if "Press Enter to continue" in output:
+                global paused_lock
+                paused_lock = True
 
     process.stdout.close()
     process.wait()
