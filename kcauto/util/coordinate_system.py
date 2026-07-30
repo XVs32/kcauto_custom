877a7214d44f4621bf9da8f62a3aece54dc3d2dc
coding21Years <coding21Years@gmail.com>
2026-07-30 08:54:32 +0800

    feat: add factory build slot 3 and 4 regions

--- kcauto/util/coordinate_system.py
+++ kcauto/util/coordinate_system.py
@@ -1,4 +1,3 @@
-
 import cv2
 import numpy as np
 import time
@@ -19,8 +18,10 @@ from constants import (
     SLEEP_MODIFIER,
 )
 
+
 class CoordinateSystem(object):
     """coordinate system class."""
+
     BROWSER_REF_ENTROPY_THRESHOLD = 0.5
     BROWSER_REF_SIZE = 100
     KC_REF_OFFSET = (-144, 0)
@@ -41,7 +42,6 @@ class CoordinateSystem(object):
     GAME_TO_WHOLE = 3
     VIEWPORT_TO_GAME = 4
     GAME_TO_VIEWPORT = 5
-    
 
     def __init__(self):
         pass
@@ -260,7 +260,6 @@ class CoordinateSystem(object):
         Log.log_msg(f"Saved {saved_count} browser refs to {debug_dir}")
         return debug_dir
 
-
     def _debug_draw_browser_slide_windows(self, screenshot_gray):
         """Draw slide windows and ref regions on the browser screenshot."""
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -318,8 +317,6 @@ class CoordinateSystem(object):
         probabilities = probabilities[probabilities > 0]
         return float(-np.sum(probabilities * np.log2(probabilities)))
 
-
-
     def convert(self, src, mode):
         dst = Region()
         dst.w = src.w
@@ -331,9 +328,9 @@ class CoordinateSystem(object):
         elif mode == self.VIEWPORT_TO_WHOLE:
             dst.x = src.x + self.viewport_x
             dst.y = src.y + self.viewport_y
-            return  dst
+            return dst
         elif mode == self.WHOLE_TO_GAME:
-            dst.x = src.x -  self.game_x
+            dst.x = src.x - self.game_x
             dst.y = src.y - self.game_y
             return dst
         elif mode == self.GAME_TO_WHOLE:
@@ -419,8 +416,16 @@ class CoordinateSystem(object):
         self._create_or_shift_region(
             "build_slot_2_stat_region", x + 595, y + 305, 150, 60
         )
+        self._create_or_shift_region(
+            "build_slot_3_stat_region", x + 595, y + 430, 150, 60
+        )
+        self._create_or_shift_region(
+            "build_slot_4_stat_region", x + 595, y + 555, 150, 60
+        )
         self._create_or_shift_region("build_slot_1_region", x + 900, y + 260, 60, 15)
         self._create_or_shift_region("build_slot_2_region", x + 900, y + 380, 60, 15)
+        self._create_or_shift_region("build_slot_3_region", x + 900, y + 500, 60, 15)
+        self._create_or_shift_region("build_slot_4_region", x + 900, y + 620, 60, 15)
         self._create_or_shift_region("order_confirm_region", x + 975, y + 635, 200, 50)
         self._create_or_shift_region("use_item_region", x + 635, y + 580, 100, 20)
         self._create_or_shift_region("develop_region", x + 215, y + 480, 200, 50)
@@ -518,5 +523,6 @@ class CoordinateSystem(object):
             return region
         else:
             raise TypeError("Invalid region specified.")
+
 
 coor = CoordinateSystem()
